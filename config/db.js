2d6a06538fd9e595c2c7b3bbe964241e7acde2e5
Bayuajinugroho16 <aji13413@gmail.com>
2026-06-04 19:23:29 +0700

    Migrate database from Supabase to Neon

--- config/db.js
+++ config/db.js
@@ -1,12 +1,14 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  host: 'aws-1-ap-southeast-2.pooler.supabase.com',
-  port: 6543,
-  user: 'postgres.qaavjrteoqwlnxdrpygn',  // Perhatikan user format
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: 'postgres',
-  ssl: { rejectUnauthorized: false }
+  database: process.env.DB_NAME,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 module.exports = pool;
